259fa80afef6d29f632f5bce4adef03b0a083b81
kuwa312 <kwhrhtk@gmail.com>
2025-05-09 11:59:48 +0900

    7A.cを更新

--- 7A.c
+++ 7A.c
@@ -17,6 +17,7 @@ int get_depth(int id) {
     int depth = 0;
     while (T[id].parent != NIL) {//接点の親がNILでない限り
         id = T[id].parent;
+    
         depth ++;
     }
     return depth;
@@ -30,19 +31,13 @@ int get_depth(int id) {
 
 
 void print_child(int id) {
+    int i, c;
     printf("[");
-    if (T[id].left_child != NIL) {
-        printf("]\n");
-        return;
-    }
-    printf("%d",T[id].left_child);
-    int c = T[id].left_child;
-    while(T[c].right_bro != NIL) {
-        c = T[c].right_bro;
-        printf(",%d", c);
+    for (i=0, c=T[id].left_child; c!=NIL; i++, c=T[c].right_bro) {
+        if (i) {printf(", ");}// i=0のとき（先頭の子のとき）は「，」を表示しない
+        printf("%d", c);
     }
     printf("]\n");
-
 }
 
 
@@ -66,14 +61,16 @@ int main(void) {
     int i, n, id, k, j, c;
     int pre_c = NIL;
     scanf("%d", &n);
+
     // 初期化
     for (i=0; i<n; i++) {
         T[i].parent = T[i].left_child = T[i].right_bro = NIL;
     }
 
+    // 多分あってる
     for (i=0; i<n; i++) {
         scanf("%d", &id);
-        scanf("%d", &k);
+        scanf("%d", &k); // K:子の次数
         for (j=0; j<k; j++) { 
             scanf("%d", &c); //c_j
             if (j==0) {// 一つ目の子がleft_child
